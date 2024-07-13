47b7fb27a4ba519dfd24a2f5b5e6d31e10f246ca
Thomas Lamant <tom@tmlmt.com>
2024-07-13 23:22:11 +0200

    test: replace pytest-snapshot by inline-snapshot

--- milk_tracker/tests/test_models_memories.py
+++ milk_tracker/tests/test_models_memories.py
@@ -1,6 +1,8 @@
+import datetime
 from pathlib import Path
 
 import pytest
+from inline_snapshot import snapshot
 from models.memories import MemoriesDataModel
 from schemas.memory import Memory
 
@@ -13,13 +15,44 @@ def m() -> MemoriesDataModel:
     return MemoriesDataModel(fixture_file_path)
 
 
-def test_init_memories(m: MemoriesDataModel, snapshot: list) -> None:
+def test_init_memories(m: MemoriesDataModel) -> None:
     """Test constructor."""
-    assert m.df.to_dict() == snapshot
+    assert m.df.to_dict("records") == snapshot(
+        [
+            {
+                "date": datetime.date(2024, 3, 1),
+                "description": "Yes, it did",
+                "index": 0,
+            },
+            {
+                "date": datetime.date(2024, 2, 1),
+                "description": "I can't believe it happened!",
+                "index": 1,
+            },
+        ]
+    )
 
 
-def test_add_row(m: MemoriesDataModel, snapshot: list) -> None:
+def test_add_row(m: MemoriesDataModel) -> None:
     """Test add_row()."""
     new_m = Memory(date="2024-02-15", description="A new memory")
     m.add(new_m)
-    assert m.df.to_dict() == snapshot
+    assert m.df.to_dict("records") == snapshot(
+        [
+            {
+                "date": datetime.date(2024, 3, 1),
+                "description": "Yes, it did",
+                "index": 0,
+            },
+            {
+                "date": datetime.date(2024, 2, 15),
+                "description": "A new memory",
+                "index": 1,
+            },
+            {
+                "date": datetime.date(2024, 2, 1),
+                "description": "I can't believe it happened!",
+                "index": 2,
+            },
+        ]
+    )
